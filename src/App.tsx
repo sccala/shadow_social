506ccc2b8688b352d02c043a4a249dd25c0465f6
sccala <sccala18@gmail.com>
2021-08-20 23:51:54 -0700

    applied tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 function App() {
   return (
-    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <img
-        className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
-        src="/img/erin-lindford.jpg"
-        alt="Woman's Face"
-      />
-      <div className="text-center space-y-2 sm:text-left">
-        <div className="space-y-0.5">
-          <p className="text-lg text-black font-semibold">Erin Lindford</p>
-          <p className="text-gray-500 font-medium">Product Engineer</p>
+    <>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:flex-shrink-0">
+            <img
+              className="h-48 w-full object-cover md:w-48"
+              src="https://source.unsplash.com/random"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Case study
+            </div>
+            <a
+              href="#"
+              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Finding customers for your new business
+            </a>
+            <p className="mt-2 text-gray-500">
+              Getting a new business off the ground is a lot of hard work. Here are five ideas you
+              can use to find your first customers.
+            </p>
+          </div>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Message
-        </button>
       </div>
-    </div>
+    </>
   )
 }
 
