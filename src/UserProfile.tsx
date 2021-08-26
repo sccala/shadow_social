65a2d554a7c2f6d69969604a706b3a407c764dc4
sccala <sccala18@gmail.com>
2021-08-26 14:34:04 -0700

    env updated

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -4,13 +4,14 @@ import { User } from './types/user'
 type Props = { user: User}
 
 export const UserProfile: VFC<Props> = (props) => {
-  const { user } = props
+  const {user} = props
   return (
     <dl>
       <dt>Name</dt>
-      <dd>{user.name}</dd>
+       <dd>{user.name}</dd>
       <dt>Hobby </dt>
-      <dd> {user.hobbies?.join('/')} </dd>
+      <dd> {user.hobbies?.join('/')}
+      </dd>
     </dl>
   )
 }
