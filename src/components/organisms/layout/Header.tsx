dcff92e2b989e41866a1820782bcea1f849f51b3
sccala <sccala18@gmail.com>
2021-08-30 15:24:30 -0700

    added home page

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -28,7 +28,7 @@ export const Header: VFC = memo(() => {
             <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
           </svg>
           <span className='ml-3 text-xl cursor-pointer' onClick={onClickHome}>
-            GhostChat
+            ShadowChat
           </span>
         </div>
         <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center cursor-pointer'>
@@ -40,7 +40,7 @@ export const Header: VFC = memo(() => {
           </div>
           <div className='mr-5'>Third Link</div>
         </nav>
-        <button className='inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0'>
+        <button className=' items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0'>
           Logout
         </button>
       </div>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,27 +1,35 @@
 import { VFC, memo } from 'react'
-import { PrimaryButton } from '../atoms/button/PrimaryButton'
 
 export const Home: VFC = memo(() => {
   return (
     <>
-        <div className='container mx-auto  flex-col md:items-start md:text-left mt-16 mb-16 md:mb-0 items-center text-center'>
-          <h1 className='title-font sm:text-3xl text-3xl mb-4 font-medium text-gray-900'>
-            Hello 
-          </h1>
-          <p className='mb-8 leading-relaxed'>
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed
-            tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken
-            authentic tumeric truffaut hexagon try-hard chambray.
-          </p>
-          <div className='flex justify-center'>
-            <button className='inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
-              Button
-            </button>
-            <button className='ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg'>
-              Button
-            </button>
+      <div className='bg-white'>
+        <div
+          className='max-w-4xl 
+        mx-auto px-4 grid items-center md:grid-cols-2 xs:grid-cols-1 
+        sm:px-8 lg:px-8 
+        xs:py-16 md:py-32 
+        lg:max-w-7xl lg:grid-cols-2 gap-4 sm:gap-6 lg:gap-6'
+        >
+          <div>
+            <h2 className='text-lg font-medium tracking-tight text-gray-900 sm:text-4xl xs:text-2xl xs:text-center'>
+              Welcome to the Shadow Chat.
+            </h2>
+            <p className='mt-4 text-gray-500 xs:text-center'>
+              The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards.
+              The powder coated steel divider separates active cards from new ones, or can be used
+              to archive important task lists.
+            </p>
+          </div>
+          <div className='gap-2 sm:gap-6 lg:gap-4'>
+            <img
+              src='/asset/images/clearbox.jpg'
+              alt='shadow on eyes'
+              className='bg-gray-100 content-center rounded-lg'
+            />
           </div>
         </div>
+      </div>
     </>
   )
 })
