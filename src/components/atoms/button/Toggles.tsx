90bafe2bfce87309305c789993ece5d78ce53a8c
sccala <sccala18@gmail.com>
2021-10-03 22:56:01 -0700

    darkmode wip

--- src/components/atoms/button/Toggles.tsx
+++ src/components/atoms/button/Toggles.tsx
@@ -1,27 +1,30 @@
-import { VFC, memo } from 'react'
-import { useDarkmode, useChangeFont } from '../../../hooks/useToggle'
+import { VFC, memo, useContext } from 'react'
+import {  useChangeFont } from '../../../hooks/useToggle'
 
 type Props = {
   className?: string
 }
 
 export const DarkModeToggle: VFC<Props> = memo(() => {
-  const { darkmode, setDarkmode } = useDarkmode()
-  const onClickToggle = () => setDarkmode(v => !v)
+  const { theme, changeTheme } = useContext(ThemeContext)
+
+  // const { darkmode, setDarkmode } = useDarkmode()
+  const onClickToggle = () => changeTheme(v => !v)
   return (
     <div onClick={onClickToggle} className='flex flex-col'>
       <label htmlFor='unchecked' className='mt-3 inline-flex items-center cursor-pointer'>
         <span className='relative'>
           <span className='block w-10 h-6 bg-gray-200 rounded-full shadow-inner'></span>
+
           <span
             className={`${
-              darkmode ? 'bg-indigo-400 transform translate-x-full' : 'bg-white'
+              theme === 'dark' ? 'bg-indigo-400 transform translate-x-full' : 'bg-white'
             } absolute block w-4 h-4 mt-1 ml-1  rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out`}
           >
             <input id='darkmode' type='checkbox' className='absolute opacity-0 w-0 h-0' />
           </span>
         </span>
-        <span className='ml-3 text-sm'>{darkmode ? 'ON' : 'OFF'}</span>
+        <span className='ml-3 text-sm'>{theme === 'dark' ? 'ON' : 'OFF'}</span>
       </label>
     </div>
   )
---
+++ src/context/themeContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useEffect } from 'react'
+
+type ThemeName = 'light' | 'dark' | string
+type ThemeContextType = {
+  theme: ThemeName
+  setTheme: (name: ThemeName) => void
+}
+
+const getInitialTheme = () => {
+  // Check if we already have a stored value in the localStorage. If not check the media query if the user browser prefers a dark or light color scheme using prefers-color-scheme media query.
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme')
+    if (typeof storedPrefs === 'string') {
+      return storedPrefs
+    }
+
+    const userMedia = window.matchMedia('(prefers-color-scheme:dark)')
+    if (userMedia.matches) {
+      return 'dark'
+    }
+  }
+  // returning default theme here
+  return 'light'
+}
+
+const ThemeContext = createContext<ThemeContextType>(null)
+
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme)
+
+  const rawSetTheme = rawTheme => {
+    const root = window.document.documentElement
+    const isDark = theme === 'dark'
+
+    root.classList.remove(isDark ? 'light' : 'dark')
+    root.classList.add(rawTheme)
+
+    localStorage.setItem('color-theme', rawTheme)
+  }
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme)
+  }
+
+  useEffect(() => {
+    rawSetTheme(theme)
+  }, [theme])
+
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>
+}
