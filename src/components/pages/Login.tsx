837e59f03da972c6e5bd7485ceed028f7eb8159b
sccala <sccala18@gmail.com>
2021-10-07 15:08:50 -0700

    fixed login darkmode

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,7 +2,7 @@ import { VFC, memo, useState, ChangeEvent, useContext } from 'react'
 import { ThemeContext } from '../../hooks/providers/themeContext'
 // import { ThemeProvider } from '../../hooks/providers/themeContext'
 import { useAuth } from '../../hooks/useAuth'
-import { PrimaryButton } from '../atoms/button/PrimaryButton'
+import { SecondaryButton } from '../atoms/button/SecondaryButton'
 
 export const Login: VFC = memo(() => {
   const [userId, setUserId] = useState<string>('')
@@ -13,9 +13,10 @@ export const Login: VFC = memo(() => {
 
   return (
     <>
-      <div className='min-h-screen bg-indigo-500 dark:bg-indigo-900 flex-col justify-center items-center'>
+      <div className='w-full min-h-screen bg-indigo-500 dark:bg-indigo-900'>
+        <div className='flex justify-center items-center'>
           <button
-            className='border-0 py-1 text-primary hover:text-accent rounded text-base pt-8 mx-auto sm:mt-0 mb-4'
+            className=' border-0 py-1 text-primary hover:text-accent rounded text-base pt-8 mx-auto sm:mt-0 mb-4'
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
           >
             <svg
@@ -37,38 +38,36 @@ export const Login: VFC = memo(() => {
               />
             </svg>
           </button>
+        </div>
         <div className='flex justify-center items-center'>
-          <div className='p-10 xs:p-0 mx-auto md:w-full md:max-w-md'>
-            <div className='py-12 px-12 bg-indigo-200 dark:bg-indigo-400 rounded-2xl shadow-xl z-20'>
-              <div>
-                <h1 className='text-2xl font-bold text-center mb-4 cursor-pointer text-primary'>
-                  Welcome Back!
-                </h1>
-                <p className='w-80 text-center text-sm mb-8 font-semibold text-primary tracking-wide cursor-pointer'>
-                  Sign in with your user ID! People are waiting for you.
-                </p>
-              </div>
-              <div className='space-y-4'>
-                <p className='text-center text-primary'>User ID</p>
-                <input
-                  type='text'
-                  placeholder='Enter 1 for user, 10 for admin demo'
-                  className='block text-sm py-3 px-4 rounded-lg w-full border outline-none'
-                  value={userId}
-                  onChange={onCangeUserId}
-                />
-              </div>
-              <div className='text-center mt-6'>
-                <PrimaryButton disabled={userId === ''} onClick={onClickLogin}>
-                  Sign in
-                </PrimaryButton>
-                <p className='mt-4 text-sm text-primary'>
-                  You don't have an account yet?
-                  <span className='underline cursor-pointer'> Sign Up</span>
-                </p>
-              </div>
+          <div className='py-12 px-12 bg-white rounded-2xl shadow-xl z-20'>
+            <div>
+              <h1 className='text-3xl font-bold text-center mb-4 cursor-pointer'>Welcome Back!</h1>
+              <p className='w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer'>
+                Sign in with your user ID! People are waiting for you.
+              </p>
+            </div>
+            <div className='space-y-4'>
+              <input
+                type='text'
+                placeholder='Email Addres'
+                className='block text-sm py-3 px-4 rounded-lg w-full border outline-none'
+                value={userId}
+                onChange={onCangeUserId}
+              />
+            </div>
+            <div className='text-center mt-6'>
+              <SecondaryButton disabled={userId === ''} onClick={onClickLogin}>
+                Sign in
+              </SecondaryButton>
+              <p className='mt-4 text-sm'>
+                You don't have an account yet?
+                <span className='underline cursor-pointer'> Sign Up</span>
+              </p>
             </div>
           </div>
+          <div className='w-40 h-40 absolute bg-purple-300 rounded-full top-0 right-12 hidden md:block'></div>
+          <div className='w-20 h-40 absolute bg-purple-300 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block'></div>
         </div>
       </div>
     </>
