12c7efad5e6e1b8cae05df27119fd31a3d30573c
sccala <sccala18@gmail.com>
2021-10-06 23:52:02 -0700

    table styling

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Shadow Chat App 
+## Introduction
 This is a social app for people who wants to whisper. Comments will be disappeared automatically after certain given time. 
+## Features
+- Create, read, delete, update chat comment
+- User and admin login
+- Dark mode
+- Reaction to the chat
+- Reliable UI design
+- Responsive
+- Editable user information
+
 # Develpoment
 ## Tools and Technologies
 - React
--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -4,36 +4,39 @@ import { DarkModeToggle, ChangeFontToggle } from '../atoms/button/Toggles'
 export const Setting: VFC = memo(() => {
   return (
     <>
-      <div className='dark:bg-primary bg-white py-20'>
-        <div className='container mx-auto '>
+      <div className='bg-primary py-20'>
+        <div className='w-2/3 mx-auto'>
           {/* setting  */}
-          <div className='bg-primary px-4 rounded-md w-full'>
-            <div className='flex justify-between w-full'></div>
-            <table className='table-auto border-collapse w-full'>
+          <div className=' bg-secondary shadow-md rounded-md my-6'>
+            <table className='text-left w-full border-collapse '>
               <thead>
-                <tr className='text-sm font-medium text-primary text-left'>
-                  <th className='px-4 py-2 bg-gray-200 dark:bg-indigo-700'>Setting</th>
-                  <th className='px-4 py-2 bg-gray-200 dark:bg-indigo-700'>ON / OFF</th>
+                <tr>
+                  <th className='text-sm font-medium text-primary text-left px-4 py-2 bg-secondary rounded-tl-md'>
+                    Setting
+                  </th>
+                  <th className='text-sm font-medium text-primary text-left px-4 py-2 bg-secondary rounded-tr-md'>
+                    ON / OFF
+                  </th>
                 </tr>
               </thead>
               <tbody className='text-sm font-normal text-primary'>
-                <tr className='hover:bg-gray-200 dark:hover:bg-gray-700 border-b border-gray-200'>
+                <tr className='hover:bg-gray-100 dark:hover:bg-gray-700 border-b border-gray-200'>
                   <td className='px-4 py-4'>Dark Mode</td>
                   <td className='px-4 py-4'>
                     <DarkModeToggle />
                   </td>
                 </tr>
-                <tr className='hover:hover:bg-gray-200 border-b border-gray-200 dark:hover:bg-gray-700'>
+                <tr className='hover:bg-gray-100  border-b border-gray-200 dark:hover:bg-gray-700'>
                   <td className='px-4 py-4'>Change Font</td>
                   <td className='px-4 py-4'>
                     <ChangeFontToggle />
                   </td>
                 </tr>
-                <tr className='hover:bg-gray-200 border-b border-gray-200 dark:hover:bg-gray-700'>
+                <tr className='hover:bg-gray-100  border-b border-gray-200 dark:hover:bg-gray-700'>
                   <td className='px-4 py-4'>Sound</td>
                   <td className='px-4 py-4'>Coming Soon</td>
                 </tr>
-                <tr className='hover:bg-gray-200 border-b border-gray-200 dark:hover:bg-gray-700'>
+                <tr className='hover:bg-gray-100 hover:rounded-b-md border-b border-gray-200 dark:hover:bg-gray-700'>
                   <td className='px-4 py-4'>Reduce graphical motion to save evergy</td>
                   <td className='px-4 py-4'>Coming Soon</td>
                 </tr>
